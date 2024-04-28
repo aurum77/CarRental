83c607e2e67955f39f1b1734fd16c8b159370557
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-04-28 22:41:10 +0300

    implement GetVehicleById method in VehicleService

--- CarRental.Application/Abstractions/IVehicleService.cs
+++ CarRental.Application/Abstractions/IVehicleService.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarRental.Domain.Entities;
 
 namespace CarRental.Application.Abstractions;
 
-public interface IVehicleService { }
+public interface IVehicleService
+{
+    Task<Vehicle> GetVehicleById(string id);
+}
--- CarRental.Persistence/Services/VehicleService.cs
+++ CarRental.Persistence/Services/VehicleService.cs
@@ -3,7 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarRental.Application.Repositories.Vehicle;
+using CarRental.Domain.Entities;
 
 namespace CarRental.Application.Abstractions.Services;
 
-public class VehicleService : IVehicleService { }
+public class VehicleService : IVehicleService
+{
+    private readonly IVehicleReadRepository _vehicleReadRepository;
+
+    public VehicleService(IVehicleReadRepository vehicleReadRepository)
+    {
+        _vehicleReadRepository = vehicleReadRepository;
+    }
+
+    public async Task<Vehicle> GetVehicleById(string id)
+    {
+        var vehicle = await _vehicleReadRepository.GetByIdAsync(id);
+
+        if (vehicle == null)
+        {
+            throw new Exception("Requested Vehicle not found");
+        }
+
+        return vehicle;
+    }
+}
