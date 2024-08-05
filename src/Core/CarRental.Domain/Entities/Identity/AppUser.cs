5940ec49c4696a65fdf59b8551f49ca08a2a76b5
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-08-05 18:17:10 +0300

    fix: add NameSurname property to appuser

--- src/Core/CarRental.Domain/Entities/Identity/AppUser.cs
+++ src/Core/CarRental.Domain/Entities/Identity/AppUser.cs
@@ -4,6 +4,7 @@ namespace CarRental.Domain.Entities.Identity
 {
     public class AppUser : IdentityUser<Guid>
     {
+        public required string NameSurname { get; set; }
         public ICollection<Rental>? Rentals { get; set; }
     }
 }
