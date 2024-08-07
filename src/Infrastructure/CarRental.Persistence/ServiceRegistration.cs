967a55b2e3d9eaa7de0d24ea1912ec328c7f623f
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-08-07 17:37:20 +0300

    add persistence services to IoC container

--- src/Infrastructure/CarRental.Persistence/ServiceRegistration.cs
+++ src/Infrastructure/CarRental.Persistence/ServiceRegistration.cs
@@ -12,5 +12,23 @@ public static class ServiceRegistration
         {
             options.UseNpgsql(Configuration.ConnectionString);
         });
+
+        services.AddScoped<IBrandReadRepository, BrandReadRepository>();
+        services.AddScoped<IBrandWriteRepository, BrandWriteRepository>();
+
+        services.AddScoped<IFeaturesReadRepository, FeaturesReadRepository>();
+        services.AddScoped<IFeaturesWriteRepository, FeaturesWriteRepository>();
+
+        services.AddScoped<IModelReadRepository, ModelReadRepository>();
+        services.AddScoped<IModelReadRepository, ModelReadRepository>();
+
+        services.AddScoped<IRentalReadRepository, RentalReadRepository>();
+        services.AddScoped<IRentalReadRepository, RentalReadRepository>();
+
+        services.AddScoped<IRequirementReadRepository, RequirementReadRepository>();
+        services.AddScoped<IRequirementReadRepository, RequirementReadRepository>();
+
+        services.AddScoped<IVehicleReadRepository, VehicleReadRepository>();
+        services.AddScoped<IVehicleReadRepository, VehicleReadRepository>();
     }
 }
